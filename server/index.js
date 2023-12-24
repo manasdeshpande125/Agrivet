6c75f1ea87f39af427bee138ee5404bb0cac98d0
manasdeshpande125 <87887744+manasdeshpande125@users.noreply.github.com>
2023-12-24 20:11:49 +0530

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -7,10 +7,15 @@ const customerFunctionalityRoutes = require('./routes/customerFunctionalityRoute
 const executiveRoute = require('./routes/executiveRoute');
 const registrationRoutes = require('./routes/registrationRoutes');
 const testdriveRoute = require('./routes/testdriveRoute');
-
+const client = require('prom-client');
 const app = express();
 
 //Initialization
+const promBundle = require('express-prom-bundle');
+
+const metricsMiddleware = promBundle({ includeMethod: true });
+
+app.use(metricsMiddleware);
 
 app.use(express.json());
 app.use("/vehicle", vehicleRoute);
@@ -20,9 +25,27 @@ app.use("/registration", registrationRoutes);
 app.use("/testdrive", testdriveRoute);
 
 
+//Create a Gauge metric for a hypothetical metric named 'example_metric'
+const exampleMetric = new client.Gauge({
+    name: 'example_metric',
+    help: 'An example metric for demonstration purposes',
+});
 
+// Dummy data for the metric
+const exampleMetricValue = Math.random() * 100;
+
+// Set the metric value
+exampleMetric.set(exampleMetricValue);
+
+
+
+app.get('/metrics', (req, res) => {
+    res.set('Content-Type', client.register.contentType);
+    res.end(client.register.metrics());
+});
 //Start the server at 3005 port
 
 app.listen(3005, () => {
     console.log("Server running on port 3005");
 });
+
